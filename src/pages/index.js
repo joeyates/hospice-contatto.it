2660b9fd378deed0ee5bd766141d341c8bfe1604
Joe Yates <joe.g.yates@gmail.com>
2023-01-13 13:09:37 +0100

    Query home page from DatoCMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
-const HomePage = () => {
-  return <div>Home page</div>
+import { request } from '../../lib/datocms'
+
+const HOMEPAGE_QUERY = `
+query HomePage {
+  home {
+    title
+    subtitle
+    body {
+      blocks
+      links
+      value
+    }
+    footer
+  }
+}
+`
+
+const HomePage = ({page}) => {
+  return (
+    <div>
+      <h1>{page.home.title}</h1>
+      <h2>{page.home.subtitle}</h2>
+      <h2>{page.home.footer}</h2>
+    </div>
+  )
 }
 
+const getStaticProps = async () => {
+  const page = await request({
+    query: HOMEPAGE_QUERY
+  })
+
+  return {
+    props: { page }
+  }
+}
+
+export {getStaticProps}
 export default HomePage
