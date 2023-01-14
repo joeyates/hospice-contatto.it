8e0cb6727d586deef8583d895c58381a7baf916e
Joe Yates <joe.g.yates@gmail.com>
2023-01-14 09:57:27 +0100

    Set a site-wide font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import localFont from '@next/font/local'
+
 import '@/styles/globals.css'
 
+const ubuntuFont = localFont({src: './fonts/Ubuntu-Regular.ttf'})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={ubuntuFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
