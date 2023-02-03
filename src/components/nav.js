b075395ebcfe721da024a0309f6d601a093a3340
Joe Yates <joe.g.yates@gmail.com>
2023-02-03 12:42:03 +0100

    Add menu

---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+export default () => {
+  return (
+    <nav className="svizzerina">
+      <input className="svizzerina__trigger" type="checkbox"/>
+
+      <span className="svizzerina__glyph svizzerina__glyph1"></span>
+      <span className="svizzerina__glyph svizzerina__glyph2"></span>
+      <span className="svizzerina__glyph svizzerina__glyph3"></span>
+
+      <ul className="svizzerina__menu">
+        <a href="/">
+          <li className="svizzerina__menu-item">Home</li>
+        </a>
+      </ul>
+    </nav>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import localFont from '@next/font/local'
 
+import Nav from '@/components/nav'
 import '@/styles/globals.css'
+import '@/styles/svizzerina.sass'
 
 const ubuntuFont = localFont({src: './fonts/Ubuntu-Regular.ttf'})
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={ubuntuFont.className}>
+      <Nav/>
       <Component {...pageProps} />
     </main>
   )
