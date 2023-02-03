6ef2dd8703a37a13db5b56540f455d84b0001bed
Joe Yates <joe.g.yates@gmail.com>
2023-02-03 13:10:55 +0100

    Show menu on desktop

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,33 @@
+import styles from '@/styles/Nav.module.css'
+
 const Nav = () => {
   return (
-    <nav className="svizzerina">
-      <input className="svizzerina__trigger" type="checkbox"/>
+    <>
+      <div className={styles['desktop-nav']}>
+      <nav className="horizontal-menu">
+        <ul className="svizzerina__menu">
+          <a href="/">
+            <li className="svizzerina__menu-item">Home</li>
+          </a>
+        </ul>
+      </nav>
+      </div>
+      <div className={styles['mobile-nav']}>
+      <nav className="svizzerina">
+        <input className="svizzerina__trigger" type="checkbox"/>
 
-      <span className="svizzerina__glyph svizzerina__glyph1"></span>
-      <span className="svizzerina__glyph svizzerina__glyph2"></span>
-      <span className="svizzerina__glyph svizzerina__glyph3"></span>
+        <span className="svizzerina__glyph svizzerina__glyph1"></span>
+        <span className="svizzerina__glyph svizzerina__glyph2"></span>
+        <span className="svizzerina__glyph svizzerina__glyph3"></span>
 
-      <ul className="svizzerina__menu">
-        <a href="/">
-          <li className="svizzerina__menu-item">Home</li>
-        </a>
-      </ul>
-    </nav>
+        <ul className="svizzerina__menu">
+          <a href="/">
+            <li className="svizzerina__menu-item">Home</li>
+          </a>
+        </ul>
+      </nav>
+      </div>
+    </>
   )
 }
 
