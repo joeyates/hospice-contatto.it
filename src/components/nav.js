fe35016749a66f05f425d1c0562087c378a4ad31
Joe Yates <joe.g.yates@gmail.com>
2023-02-03 13:17:33 +0100

    Correct formatting

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,28 +4,28 @@ const Nav = () => {
   return (
     <>
       <div className={styles['desktop-nav']}>
-      <nav className="horizontal-menu">
-        <ul className="svizzerina__menu">
-          <a href="/">
-            <li className="svizzerina__menu-item">Home</li>
-          </a>
-        </ul>
-      </nav>
+        <nav className="horizontal-menu">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </nav>
       </div>
       <div className={styles['mobile-nav']}>
-      <nav className="svizzerina">
-        <input className="svizzerina__trigger" type="checkbox"/>
+        <nav className="svizzerina">
+          <input className="svizzerina__trigger" type="checkbox"/>
 
-        <span className="svizzerina__glyph svizzerina__glyph1"></span>
-        <span className="svizzerina__glyph svizzerina__glyph2"></span>
-        <span className="svizzerina__glyph svizzerina__glyph3"></span>
+          <span className="svizzerina__glyph svizzerina__glyph1"></span>
+          <span className="svizzerina__glyph svizzerina__glyph2"></span>
+          <span className="svizzerina__glyph svizzerina__glyph3"></span>
 
-        <ul className="svizzerina__menu">
-          <a href="/">
-            <li className="svizzerina__menu-item">Home</li>
-          </a>
-        </ul>
-      </nav>
+          <ul className="svizzerina__menu">
+            <li className="svizzerina__menu-item">
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   )
