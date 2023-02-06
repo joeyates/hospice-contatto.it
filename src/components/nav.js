5b0b55c5e50fe1abef258e35f585054c85d8e7fa
Joe Yates <joe.g.yates@gmail.com>
2023-02-06 09:14:43 +0100

    Unite nav html

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,35 +1,22 @@
 import Link from 'next/link'
 
-import styles from '@/styles/Nav.module.css'
+import styles from './nav.module.sass'
 
 const Nav = () => {
   return (
-    <>
-      <div className={styles['desktop-nav']}>
-        <nav className="horizontal-menu">
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className={styles['mobile-nav']}>
-        <nav className="svizzerina">
-          <input className="svizzerina__trigger" type="checkbox"/>
+    <nav className="svizzerina">
+      <input className="svizzerina__trigger" type="checkbox"/>
 
-          <span className="svizzerina__glyph svizzerina__glyph1"></span>
-          <span className="svizzerina__glyph svizzerina__glyph2"></span>
-          <span className="svizzerina__glyph svizzerina__glyph3"></span>
+      <span className="svizzerina__glyph svizzerina__glyph1"></span>
+      <span className="svizzerina__glyph svizzerina__glyph2"></span>
+      <span className="svizzerina__glyph svizzerina__glyph3"></span>
 
-          <ul className="svizzerina__menu">
-            <li className="svizzerina__menu-item">
-              <Link href="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+      <ul className="svizzerina__menu">
+        <li className="svizzerina__menu-item">
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
