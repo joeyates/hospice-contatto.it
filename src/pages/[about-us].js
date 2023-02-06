d21a5a51a64b926de09e6f474ba7a5fbb35a9e4a
Joe Yates <joe.g.yates@gmail.com>
2023-02-06 10:28:14 +0100

    Add page with localized slug

---
+++ src/pages/[about-us].js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import { StructuredText } from 'react-datocms'
+
+import { request } from '../../lib/datocms'
+import styles from './about-us.module.sass'
+
+const ABOUT_US_QUERY = `
+query {
+  about {
+    id
+    title
+    body {
+      value
+    }
+  }
+}
+`
+
+const AboutUs = ({ page }) => {
+  return (
+    <>
+      <Head>
+        <title>{page.about.title}</title>
+      </Head>
+      <main className={styles.main}>
+        <h1>{page.about.title}</h1>
+      </main>
+    </>
+  )
+}
+
+const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { 'about-us': 'chi-siamo' } }],
+    fallback: false
+  }
+}
+
+const getStaticProps = async () => {
+  const page = await request({
+    query: ABOUT_US_QUERY
+  })
+
+  return {
+    props: { page }
+  }
+}
+
+export { getStaticPaths, getStaticProps }
+export default AboutUs
