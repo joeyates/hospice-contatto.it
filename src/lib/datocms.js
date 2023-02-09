8428a3c61133b0a43c62f044218ae582aaadcd6b
Joe Yates <joe.g.yates@gmail.com>
2023-02-09 19:35:45 +0100

    Remove semicolons

--- src/lib/datocms.js
+++ src/lib/datocms.js
@@ -4,16 +4,16 @@ const parseDate = date => new Date(date)
 
 const request = ({query, variables, includeDrafts, excludeInvalid}) => {
   const headers = {
-    authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
-  };
+    authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`
+  }
   if (includeDrafts) {
-    headers['X-Include-Drafts'] = 'true';
+    headers['X-Include-Drafts'] = 'true'
   }
   if (excludeInvalid) {
-    headers['X-Exclude-Invalid'] = 'true';
+    headers['X-Exclude-Invalid'] = 'true'
   }
-  const client = new GraphQLClient('https://graphql.datocms.com', {headers});
-  return client.request(query, variables);
+  const client = new GraphQLClient('https://graphql.datocms.com', {headers})
+  return client.request(query, variables)
 }
 
 export {parseDate, request}
