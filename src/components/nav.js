0a1377c1f3a6e220fe3bb229b65258026b4b8372
Joe Yates <joe.g.yates@gmail.com>
2023-02-10 12:18:51 +0100

    Hide hamburger after navigation

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import {withRouter} from 'next/router'
+import {useRouter} from 'next/router'
+import {useEffect, useState} from 'react'
 
 import styles from './nav.module.sass'
 
@@ -11,35 +12,50 @@ const linkClass = (name, current) => {
   }
 }
 
-const Nav = ({router}) => (
-  <nav className={styles.container}>
-    <input className={styles.trigger} type="checkbox"/>
+const Nav = () => {
+  const router = useRouter()
+  const [checked, setChecked] = useState(false)
 
-    <span className={styles.glyph1}></span>
-    <span className={styles.glyph2}></span>
-    <span className={styles.glyph3}></span>
+  useEffect(() => {
+    setChecked(false)
+  }, [router.asPath])
 
-    <ul className={styles.menu}>
-      <li className={linkClass('/', router.pathname)}>
-        <Link href="/">Home</Link>
-      </li>
-      <li className={linkClass('/chi-siamo', router.pathname)}>
-        <Link href="/chi-siamo">Chi siamo</Link>
-      </li>
-      <li className={linkClass('/eventi', router.pathname)}>
-        <Link href="/eventi">Eventi</Link>
-      </li>
-      <li className={linkClass('/approfondimenti', router.pathname)}>
-        <Link href="/approfondimenti">Approfondimenti</Link>
-      </li>
-      <li className={linkClass('/come-sostenerci', router.pathname)}>
-        <Link href="/come-sostenerci">Come sostenerci</Link>
-      </li>
-      <li className={linkClass('/contatti', router.pathname)}>
-        <Link href="/contatti">Contatti</Link>
-      </li>
-    </ul>
-  </nav>
-)
+  const handleChange = e => {
+    if (e.target.checked) {
+      setChecked(true)
+    }
+  }
 
-export default withRouter(Nav)
+  return (
+    <nav className={styles.container}>
+      <input className={styles.trigger} type="checkbox" checked={checked} onChange={handleChange}/>
+
+      <span className={styles.glyph1}></span>
+      <span className={styles.glyph2}></span>
+      <span className={styles.glyph3}></span>
+
+      <ul className={styles.menu}>
+        <li className={linkClass('/', router.pathname)}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={linkClass('/chi-siamo', router.pathname)}>
+          <Link href="/chi-siamo">Chi siamo</Link>
+        </li>
+        <li className={linkClass('/eventi', router.pathname)}>
+          <Link href="/eventi">Eventi</Link>
+        </li>
+        <li className={linkClass('/approfondimenti', router.pathname)}>
+          <Link href="/approfondimenti">Approfondimenti</Link>
+        </li>
+        <li className={linkClass('/come-sostenerci', router.pathname)}>
+          <Link href="/come-sostenerci">Come sostenerci</Link>
+        </li>
+        <li className={linkClass('/contatti', router.pathname)}>
+          <Link href="/contatti">Contatti</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
