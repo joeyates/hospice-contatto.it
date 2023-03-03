8a452da2f85f7405d54c8fa129b9cfdde8df43c6
Joe Yates <joe.g.yates@gmail.com>
2023-03-03 21:30:50 +0100

    Format dates with optional time component

--- app/lib/format.js
+++ app/lib/format.js
@@ -3,4 +3,21 @@ const LOCALE = 'it-IT'
 
 const date = date => date.toLocaleDateString(LOCALE, DATE_OPTIONS)
 
-export {date}
+const isDateOnly = d => d.getHours() === 0 && d.getMinutes() === 0 && d.getSeconds() === 0
+
+const dateTime = d => {
+  const s = date(d)
+  const time = d.toLocaleTimeString(LOCALE)
+  return `${s}, ${time}`
+}
+
+const dateWithOptionalTime = d => {
+  const dateOnly = isDateOnly(d)
+  if(dateOnly) {
+    return date(d)
+  } else {
+    return dateTime(d)
+  }
+}
+
+export {date, dateTime, dateWithOptionalTime}
