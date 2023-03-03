8edc173091038f1b7a0881a8cdfe0798ab67fe11
Joe Yates <joe.g.yates@gmail.com>
2023-03-03 18:56:23 +0100

    Place footer at end of short pages

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,13 @@ import './globals.sass'
 import Footer from '@components/Footer'
 import Nav from '@components/Nav'
 import {DEFAULTS} from '@lib/metadata'
+import styles from './layout.module.sass'
 
 const ubuntuFont = localFont({src: './fonts/Ubuntu-Regular.ttf'})
 
 const RootLayout = ({children}) => (
-  <html lang="it" className={ubuntuFont.className}>
-    <body>
+  <html lang="it" className={`${ubuntuFont.className} ${styles.html}`}>
+    <body className={styles.body}>
       <Nav/>
       {children}
       <Footer/>
