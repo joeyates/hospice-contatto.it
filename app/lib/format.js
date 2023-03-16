56792dbd4930572b8ab22dc6cfa5d15e25eef34a
Joe Yates <joe.g.yates@gmail.com>
2023-03-16 09:54:07 +0100

    Ensure correct time zone when localizing times

--- app/lib/format.js
+++ app/lib/format.js
@@ -1,5 +1,5 @@
 const DATE_OPTIONS = {year: 'numeric', month: 'long', day: 'numeric'}
-const TIME_OPTIONS = {hour: 'numeric', minute: 'numeric'}
+const TIME_OPTIONS = {hour: 'numeric', minute: 'numeric', timeZone: 'Europe/Rome'}
 const LOCALE = 'it-IT'
 
 const date = date => date.toLocaleDateString(LOCALE, DATE_OPTIONS)
