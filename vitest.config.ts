8f24548e40f4e6be5eb5061e00343fad95c7e466
Joe Yates <joe.g.yates@gmail.com>
2024-02-24 19:24:08 +0100

    Clean up the DOM after tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     include: ['app/**/*.spec.tsx'],
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: ['setup-vitest.ts'],
   }
 })
