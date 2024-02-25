60305844e1c1a94b458bc9229050b6c166c6f915
Joe Yates <joe.g.yates@gmail.com>
2024-02-25 12:11:38 +0100

    Add a first test that mocks GraphQL requests
    I was unable to get the MSW graphql method to intercept requests,
    so I went with http instead

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    include: ['app/**/*.spec.tsx'],
+    include: ['app/**/*.spec.ts', 'app/**/*.spec.tsx'],
     environment: 'jsdom',
     setupFiles: ['setup-vitest.ts'],
   }
